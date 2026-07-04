8947a211da87efb9212bb1c634717254125abbe7
kalistavillatoro <kalistavillatoro@gmail.com>
2026-07-04 15:05:05 -0400

    Update Stripe checkout URL in site configuration

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,3 +1,3 @@
 // Central site configuration.
 // TODO: Replace with your real Stripe checkout link before launch.
-export const STRIPE_CHECKOUT_URL = 'https://buy.stripe.com/REPLACE_ME';
+export const STRIPE_CHECKOUT_URL = 'https://buy.stripe.com/3cI5kE8X72Iob6i4gjcV200';
